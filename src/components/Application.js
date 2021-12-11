e7865e3ec6ca731a00129904686f578ae6362e74
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-12-10 19:21:11 -0500

    Add axios and make the API request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList"
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const daysAPI = '/api/days';
+    axios.get(daysAPI)
+      .then((response) => {
+        setDays(response.data);
+      })
+  }, []);
+
+  const appointment = appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />);
 
   return (
     <main className="layout">
@@ -36,8 +69,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day}
-            onChange={setDay}
+            value={days}
+            onChange={setDays}
           />
         </nav>
         <img
@@ -46,7 +79,8 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />      </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,24 @@
-import React from "react";
-// import Header from "./Header";
-// import Empty from "./Empty";
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
-  const text = props.time ? `Appointment at ${props.time}` : "No appointment";
+  // const text = props.time ? `Appointment at ${props.time}` : "No appointment";
 
   return (
-    <article className="appointment">{text}</article>
+    <Fragment>
+      <Header time={props.time} />
+      <article className="appointment">
+        {props.interview ?
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          /> :
+          <Empty />}
+      </article>
+    </Fragment>
   );
 }

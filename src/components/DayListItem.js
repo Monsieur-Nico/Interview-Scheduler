cc66b3bc7c0c16f4387299ac1b4c1981acd586ea
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-12-15 22:37:23 -0500

    Install Cypress and write first test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,7 +9,8 @@ export default function DayListItem(props) {
       "day-list__item": true,
       "day-list__item--selected": props.selected,
       "day-list__item--full": !props.spots
-    })} onClick={() => props.setDay(props.name)}>
+    })} onClick={() => props.setDay(props.name)}
+      data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       {(props.spots > 1) && <h3 className="text--light">{props.spots} spots remaining</h3>}
       {(props.spots === 1) && <h3 className="text--light">{props.spots} spot remaining</h3>}
